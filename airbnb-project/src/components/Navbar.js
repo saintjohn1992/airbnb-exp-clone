f301b8846732c6424b97245f3c8e58139043f565
saintjohn1992 <saintjohnshane@outlook.com>
2022-06-08 15:47:32 +0200

    created components folder, set up nav component along with css file and added the airbnb img

---
+++ airbnb-project/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import "./navbar.css";
+import Airbnb from "../images/airbnb-logo.png"
+
+const Navbar = () => {
+return (
+   <div className="navbar">
+       <img src={Airbnb} alt="" className="logo" />
+   </div> 
+)
+}
+
+export default Navbar; 
