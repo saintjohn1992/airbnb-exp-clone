b7a6173912500582627512e3098ab34e85b5bc3e
saintjohn1992 <saintjohnshane@outlook.com>
2022-06-08 19:13:04 +0200

    finshing up the hero component and css file

--- airbnb-project/src/components/Hero/Hero.js
+++ airbnb-project/src/components/Hero/Hero.js
@@ -1,11 +1,20 @@
 import "./hero.css";
-import Photogrid from "../../images/hero.png";
+import Photogrid from "../../images/Peek.png";
 
 
 const Hero = () => {
 
     return (
-        <div>hero</div>
+        <div className="hero-container">
+            <img src={Photogrid} alt="" className="hero" />
+
+            <h1 className="hero-title">Online Experiences</h1>
+
+            <p className="h-desc">
+            Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+
+            </p>
+        </div>
     )
 }
 
