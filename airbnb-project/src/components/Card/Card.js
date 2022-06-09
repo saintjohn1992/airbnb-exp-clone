d8908af8d26e23ff6feb86f5ed3d1961e74bce0c
saintjohn1992 <saintjohnshane@outlook.com>
2022-06-09 15:52:20 +0200

    almost done setting up the card, going to add props to it

--- airbnb-project/src/components/Card/Card.js
+++ airbnb-project/src/components/Card/Card.js
@@ -1,11 +1,21 @@
 import "./card.css"
+import Katie from "../../images/Katie-Zaferes.jpeg"
+import Star from "../../images/star.png"
 
 
 const Card = () => {
     
     return (
         <div className="card-container">
-            card
+         <img src={Katie} alt="" className="c-img" />
+         <div className="c-template">
+            <img src={Star} alt="" className="rating" />
+                <span className="gray">5.0 </span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
         </div>
     )
 }
